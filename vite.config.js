0e51582c52e96cb429ed286fc66d385b8c8e150d
EDARA ANITHA <185475127+AnithaEdara@users.noreply.github.com>
2025-09-22 19:25:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './job-craft',
+  base: '/job-craft/',   
   server: {
-    port: 3000, // change this to the port number you want to use
-    host: true  // this enables access from your AWS public DNS
+    port: 3000,
+    host: true
   }
 })
